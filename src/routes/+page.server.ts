6c470358e23401a905940c431e6b5aedb8276909
Perchun Pak <git@perchun.it>
2026-04-25 16:50:15 +0200

    Create initial form

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,31 @@
+import { superValidate, message } from "sveltekit-superforms";
+import { valibot } from "sveltekit-superforms/adapters";
+import { fail } from "@sveltejs/kit";
+import * as v from "valibot";
+
+const schema = v.object({
+  url: v.pipe(v.string(), v.url()),
+  introduction: v.optional(v.string()),
+});
+
+export const load = async () => {
+  const form = await superValidate(valibot(schema));
+  return { form };
+};
+
+export const actions = {
+  // @ts-expect-error request is any
+  default: async ({ request }) => {
+    const form = await superValidate(request, valibot(schema));
+
+    if (!form.valid) {
+      // Return { form } and things will just work.
+      return fail(400, { form });
+    }
+
+    // TODO: Do something with the validated form.data
+
+    // Return the form with a status message
+    return message(form, "Saved successfully!");
+  },
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { page } from "$app/state";
+  import { enhance } from "$app/forms";
+
+  let { form } = $props();
+
+  let message = $derived(form?.form?.message);
+  let errors = $derived(form?.form?.errors ?? {});
+
+  $effect(() => {
+    // Focus on first error
+    if (Object.keys(errors).length) {
+      document.querySelector<HTMLElement>('[aria-invalid="true"]')?.focus();
+    }
+  });
+</script>
+
+{#if message}
+  <div class="status" class:error={page.status >= 400} class:success={page.status == 200}>
+    {message}
+  </div>
+{/if}
+
+<form method="POST" use:enhance class="w-full max-w-md space-y-4 p-4">
+  <fieldset class="space-y-4">
+    <!-- URL -->
+    <label class="label">
+      <span class="label-text">Job URL</span>
+      <input
+        name="url"
+        type="url"
+        class="input"
+        placeholder="https://startupjobs.com/..."
+        aria-invalid={errors.url ? "true" : undefined}
+      />
+      {#if errors.url}<span class="text-error-500">{errors.url}</span>{/if}
+    </label>
+    <!-- Textarea -->
+    <label class="label">
+      <span class="label-text">Textarea</span>
+      <textarea
+        name="introduction"
+        class="textarea rounded-container"
+        rows="4"
+        aria-invalid={errors.introduction ? "true" : undefined}
+      ></textarea>
+      {#if errors.introduction}<span class="text-error-500">{errors.introduction}</span>{/if}
+    </label>
+  </fieldset>
+  <fieldset class="flex justify-end">
+    <!-- Button -->
+    <button type="submit" class="btn preset-outlined-surface-300-700">Submit</button>
+  </fieldset>
+</form>
